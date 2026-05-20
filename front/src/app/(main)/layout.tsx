b282ddf26822f945bc8e8afd7727d3e76ccc1faa
clement lmd <clement.laumond@gmail.com>
2026-05-20 15:41:30 +0200

    fix websocket notifs on all pages

--- front/src/app/(main)/layout.tsx
+++ front/src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import { HomeNavbar } from "@/components/layout/HomeNavbar";
 import { SiteFooter } from "@/components/home/SiteFooter";
 import { UnreadCountProvider } from "@/contexts/UnreadCountContext";
+import { MessagesSocketProvider } from "@/contexts/MessagesSocketContext";
 
 export default function MainLayout({
   children,
@@ -9,11 +10,13 @@ export default function MainLayout({
 }) {
   return (
     <UnreadCountProvider>
-      <div className="min-h-screen flex flex-col">
-        <HomeNavbar />
-        <main className="flex-1 pt-12 md:pt-20">{children}</main>
-        <SiteFooter />
-      </div>
+      <MessagesSocketProvider>
+        <div className="min-h-screen flex flex-col">
+          <HomeNavbar />
+          <main className="flex-1 pt-12 md:pt-20">{children}</main>
+          <SiteFooter />
+        </div>
+      </MessagesSocketProvider>
     </UnreadCountProvider>
   );
 }
